63c8f5b77e1753a8fc3ffe32bfc11729e31310cc
myb <mmyybb3000@gmail.com>
2018-08-20 22:09:59 +0800

    解决跨域问题

--- src/main.js
+++ src/main.js
@@ -77,6 +77,7 @@ Vue.config.devtools = true
 
 Vue.http.interceptors.push((request, next) => {
   Vue.$vux.loading.show({text: '加载中'})
+  request.credentials = true // 传递cookie
   next((response) => { // 在响应之后传给then之前对response进行修改和逻辑判断。对于token时候已过期的判断，就添加在此处，页面中任何一次http请求都会先调用此处方法
     Vue.$vux.loading.hide()
     if (response.status === 0) {
